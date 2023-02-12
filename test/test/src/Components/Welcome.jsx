20b2ab5db6cbdc51819e3984f0e5066d0436e3b4
adcontreras <adcontrerasmm@gmail.com>
2023-02-11 20:35:16 -0600

    Home

---
+++ test/test/src/Components/Welcome.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+
+export default function Welcome() {
+
+    return(
+       <>
+        <div className="container">
+            <h6 className='header'>[Making your life Easier]</h6>
+            <h1 className='title'>Discovering the World</h1>
+        </div>  
+        <div className='container text-center'>
+        <div className="row align-items-start">
+          <div className="col">
+            All
+          </div>
+          <div className="col">
+            Travel
+          </div>
+          <div className="col">
+            Lifestyle
+          </div>
+          <div className="col">
+            Bussines
+          </div>
+          <div className="col">
+            Food
+          </div>
+          <div className="col">
+            Work
+          </div>
+        </div>
+      </div>
+
+      </>
+    )
+}
+
--- test/test/src/main.jsx
+++ test/test/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import './App.css'
+import Welcome from './Components/Welcome';
+import Images from './Components/Images';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Welcome />
+    <Images />
+
   </React.StrictMode>,
 )
